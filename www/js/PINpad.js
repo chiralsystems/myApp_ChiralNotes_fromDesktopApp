97f3dbb33cd722fcb73641402fe45d2203aa8b54
chiralsystems <dominic@chiralsystems.com>
2015-04-15 11:46:00 +0100

    Chiral Form - V1.0
    Initial Commit

---
+++ www/js/PINpad.js
@@ -0,0 +1,3 @@
+function addNumber(element){ document.getElementById('PINbox').value = document.getElementById('PINbox').value+element.value; }
+function clearForm(element){ document.getElementById('PINbox').value = ""; }
+function submitForm(element) { if (element.value == "") { alert("Enter a PIN"); } else { alert("Your PIN has been send! - "+element.value); document.getElementById('PINbox').value = ""; }; };
